06affbd8cdcfa6275cce81309d35811c2f46c8e0
Jonty Davis <jontyjont@btinternet.com>
2013-02-09 05:41:34 +0000

    first commit of sallyport2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sallyport2::Application.config.secret_token = '2cde1b0ffdeb0bf4ff12539c9acf25aff390c4491521d107128cd7a8942ef4a1dc23c381524dea4ae895bc121c812382e84212551a013c4efd24c13ef835ec8e'
