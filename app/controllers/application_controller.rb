206f199b8f45957d3efc37e6ef873c7f9aab0160
Jonty Davis <jontyjont@btinternet.com>
2013-02-24 17:57:25 +0000

    changed slider to responsive slider basic style and colours in place

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :get_dances
+
+
+  def get_dances
+  	 @dances = Blurb.where(:cat => 'dance')
+  end
 end
