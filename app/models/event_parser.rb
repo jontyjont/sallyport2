3f081323b61bb7411705b9908fe60e1a7f4bfd63
Jonty Davis <jontyjont@btinternet.com>
2013-05-04 09:03:46 +0100

    improving the grammar

--- app/models/event_parser.rb
+++ app/models/event_parser.rb
@@ -9,6 +9,8 @@ GRAMMAR = {practice: /^\s?practice/i,
 		   dk: /^\s*dk:/i,
 		   prob: /^\s*prob:/i,
 		   booking: /^\s*booking(s)?:/i,
+		   booking2: /(^\s*.+,)(\s*.+\s+\d+)(.+\d+)?\./i,
+		   dert: /^(\s*dert\s*)(\d{4})/i,
 		   empty: /^\s*$/,
 		   end_section: /^=+$/
 		   }
@@ -32,20 +34,31 @@ def parse
 		next_line = @blurb[i+1] if (i < (@blurb.length - 1))
 		
 		puts line.inspect
-		
-		
-		
+				
 		case line
 		when reg(:practice)
 			flag = :p
 		when reg(:booking)
 			flag = :b
+	    when reg(:dert)
+	    	obj.save if obj.present?
+	    	obj = Booking.new
+	    	obj.what = "DERT #{$~[1]}"
 		when reg(:date)
 			obj.save if obj.present? 
 			obj = Booking.new if flag == :b
 			obj = Practice.new if flag == :p
-			obj.when = Date.parse($~.to_s)
+			obj.when = Date.parse($~[0])
 			obj.where = $' if $'.present?
+		when reg(:booking2)
+			obj.save if obj.present? 
+			if flag == :b
+				obj = Booking.new 
+				obj.what = $~[0].to_s
+				obj.when = Date.parse($~[1])
+		    else
+			  next
+		    end
 		when reg(:yes)
 			obj.yes = $'
 			prev_line = 'yes'
@@ -66,7 +79,7 @@ def parse
 	    	obj.save
 	    	obj = nil
 		else
-	    	if prev_line.present? 
+	    	if prev_line.present? && prev_line.reverse[0] == ','
 	    		curr = obj.send(prev_line)
 	    	    obj.send(prev_line +"=", curr + line.strip)
 	    	end
