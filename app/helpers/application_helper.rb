e0bdc3b09319365e110a71e53eff8e91a83bb44f
jonty <jonty@jontyjont.plus.com>
2016-02-17 16:14:19 +0000

    responsive sallyport done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
 
 def form_fields(form, label, value)
 out=""
-out << "<div class='form-group'>
-<label class='col-sm-2 control-label'>#{label}</label>
-#{form.text_field value, :class => 'col-sm-8'}
+out << "<div class='form-group width-maxed'>
+<label>#{label}</label>
+#{form.text_field value, :class => 'form-control '}
 </div>"
 out.html_safe
 end
