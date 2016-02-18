6ab7c85658955d969bf7182236c5645fb8048265
jonty <jonty@jontyjont.plus.com>
2016-02-18 22:23:39 +0000

    typo

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -4,7 +4,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USENAME'],
+  :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD']
   
 }
